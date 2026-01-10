af628894bd6ec53be5ea978e890556c2059f8cd6
Sourav <souravbhardwaj2005@gmail.com>
2026-01-10 20:08:35 +0530

    project update

--- script.js
+++ script.js
@@ -6,3 +6,10 @@ menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
     navbar.classList.toggle('active');
 };
+fragment: (uv, mouse) => {
+  const dx = uv.x - mouse.x;
+  const dy = uv.y - mouse.y;
+  const len = Math.sqrt(dx * dx + dy * dy);
+  const displacement = smoothStep(0.4, 0.0, len);
+  return texture(uv.x + displacement * dx, uv.y + displacement * dy);
+}
